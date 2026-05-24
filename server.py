0f8d67cfc8e069b268a338fc41f565c064fc30cc
sharingchronicleofrealms-cmyk <sharing.chronicleofrealms@gmail.com>
2026-05-23 19:28:02 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,108 +1,87 @@
-import socket
-import threading
+import asyncio
+import websockets
 import json
 
-HOST = "0.0.0.0"
-PORT = 5555
-
-clients = []
-players = {}  # player_name -> simple state
+players = {}  # name -> state
 
 
-def send(sock, msg):
-    try:
-        sock.send((json.dumps(msg) + "\n").encode())
-    except:
-        pass
+async def broadcast():
+    """Send full world state to all clients"""
+    if not connected:
+        return
+
+    msg = json.dumps({
+        "type": "state",
+        "data": players
+    })
+
+    await asyncio.gather(*[
+        ws.send(msg) for ws in connected
+    ])
 
 
-def broadcast(msg):
-    for c in clients:
-        send(c, msg)
+connected = set()
 
 
-def handle_client(conn, addr):
-    print("Connected:", addr)
-    clients.append(conn)
-
+async def handler(ws):
+    connected.add(ws)
     player_name = None
 
     try:
-        buffer = ""
+        async for message in ws:
+            data = json.loads(message)
 
-        while True:
-            data = conn.recv(1024).decode()
-            if not data:
-                break
+            msg_type = data.get("type")
+            player_name = data.get("player", player_name)
 
-            buffer += data
+            # JOIN
+            if msg_type == "join":
+                players[player_name] = {
+                    "x": 0,
+                    "y": 0,
+                    "hp": 10
+                }
 
-            while "\n" in buffer:
-                line, buffer = buffer.split("\n", 1)
+            # ACTION
+            elif msg_type == "action":
+                if player_name not in players:
+                    continue
 
-                msg = json.loads(line)
+                action = data.get("data", {})
 
-                msg_type = msg.get("type")
-                player_name = msg.get("player", player_name)
+                if action.get("move") == "up":
+                    players[player_name]["y"] -= 1
+                if action.get("move") == "down":
+                    players[player_name]["y"] += 1
+                if action.get("move") == "left":
+                    players[player_name]["x"] -= 1
+                if action.get("move") == "right":
+                    players[player_name]["x"] += 1
 
-                # JOIN
-                if msg_type == "join":
-                    players[player_name] = {
-                        "x": 0,
-                        "y": 0,
-                        "hp": 10
-                    }
-
-                # ACTION (movement placeholder)
-                elif msg_type == "action":
-                    action = msg.get("data", {})
-
-                    if player_name not in players:
-                        continue
-
-                    if action.get("move") == "up":
-                        players[player_name]["y"] -= 1
-                    if action.get("move") == "down":
-                        players[player_name]["y"] += 1
-                    if action.get("move") == "left":
-                        players[player_name]["x"] -= 1
-                    if action.get("move") == "right":
-                        players[player_name]["x"] += 1
-
-                # CHAT (global for now)
-                elif msg_type == "chat":
-                    broadcast({
-                        "type": "chat",
-                        "player": player_name,
-                        "data": msg.get("data")
-                    })
-
-                # send updated world state to everyone
-                broadcast({
-                    "type": "state",
-                    "data": players
+            # CHAT
+            elif msg_type == "chat":
+                msg = json.dumps({
+                    "type": "chat",
+                    "player": player_name,
+                    "data": data.get("data")
                 })
 
-    except:
-        pass
+                await asyncio.gather(*[
+                    ws.send(msg) for ws in connected
+                ])
 
-    print("Disconnected:", addr)
-    if conn in clients:
-        clients.remove(conn)
-    conn.close()
+            await broadcast()
 
-
-def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORT))
-    server.listen()
-
-    print(f"Server running on {HOST}:{PORT}")
-
-    while True:
-        conn, addr = server.accept()
-        threading.Thread(target=handle_client, args=(conn, addr)).start()
+    finally:
+        connected.remove(ws)
+        if player_name in players:
+            del players[player_name]
 
 
-if __name__ == "__main__":
-    main()
+async def main():
+    print("WebSocket server running on port 8765")
+    async with websockets.serve(handler, "0.0.0.0", 8765):
+        await asyncio.Future()
+
+
+asyncio.run(main())
